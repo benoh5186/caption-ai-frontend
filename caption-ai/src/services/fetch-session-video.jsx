08e97304d586a7d0b2a0d82c6e850913221e96d9
Ben Oh <moh16@my.bcit.ca>
2026-03-30 20:40:23 -0700

    throw video not found error in fetch session video function

--- caption-ai/src/services/fetch-session-video.jsx
+++ caption-ai/src/services/fetch-session-video.jsx
@@ -1,11 +1,16 @@
 import { VideoLoadError } from "../errors/video-load-error"
+import { VideoNotFound } from "../errors/video-not-found"
 
 export async function fetchSessionVideo(s3Object) {
     const response = await fetch(`localhost:8000/api/load-video?${encodeURIComponent(s3Object)}`)
+    const responseStatus = response.status 
     if (response.ok) {
         const videoBlob = await response.blob()
         return URL.createObjectURL(videoBlob)
-    } else {
+    } else if (responseStatus == 404) {
+        throw new VideoNotFound("Video not found")
+    }
+    else {
         throw new VideoLoadError("Failed to load video")
     }
 }
